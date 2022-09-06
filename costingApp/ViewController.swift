3ce0b8e44f78be94332b3027a0f2da53ce95ec90
ash <ogawa.sato01@gmail.com>
2022-09-06 13:21:49 +0900

    Initial Commit

---
+++ costingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  costingApp
+//
+//  Created by 小川慧 on 2022/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
